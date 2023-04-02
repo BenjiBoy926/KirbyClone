4e0d7e99237bf2c94ea2ac623bfc72768de934ba
Codey Huntting <creativecodey@gmail.com>
2023-04-02 15:30:41 -0500

    Modified hit box
    Removed the repeating damage functionality because it might need to be moved into a separate class down the line

--- Assets/Scripts/Hitbox/Hitbox.cs
+++ Assets/Scripts/Hitbox/Hitbox.cs
@@ -5,29 +5,8 @@ using NaughtyAttributes;
 
 public class Hitbox : MonoBehaviour
 {
-    [System.Serializable]
-    public struct HitData
-    {
-        public Hurtbox _hurtbox;
-        public float _timeOfLastHit;
-
-        public float TimeSinceLastHit => Time.time - _timeOfLastHit;
-
-        public HitData(Hurtbox hurtbox, float timeOfLastHit)
-        {
-            _hurtbox = hurtbox;
-            _timeOfLastHit = timeOfLastHit;
-        }
-    }
-
     [SerializeField]
     private int _strength = 1;
-    [SerializeField]
-    private bool _hitMultipleTimes = false;
-    [SerializeField, EnableIf(nameof(_hitMultipleTimes))]
-    private float _delayBetweenHits = 0.4f;
-    
-    private Dictionary<Collider2D, HitData> _hitDatas = new Dictionary<Collider2D, HitData>();
 
     public int Strength => _strength;
 
@@ -37,33 +16,14 @@ public class Hitbox : MonoBehaviour
     {
         TryDealDamage(collision);
     }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        TryDealDamage(collision);
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        _hitDatas.Remove(collision);
-    }
-
     private void TryDealDamage(Collider2D collider)
     {
-        if (!IsAllowedToHit(collider))
-        {
-            return;
-        }
-
         Hurtbox hurtbox = collider.GetComponent<Hurtbox>();
         if (hurtbox == null)
         {
             return;
         }
-
         DealDamage(hurtbox);
-        if (_hitMultipleTimes)
-        {
-            _hitDatas[collider] = new HitData(hurtbox, Time.time);
-        }
     }
     public void DealDamage(Hurtbox hurtbox)
     {
@@ -73,9 +33,5 @@ public class Hitbox : MonoBehaviour
         }
         int adjustedDamage = hurtbox.ReceiveDamage(this);
         OnHit(this, hurtbox, adjustedDamage);
-    }
-    public bool IsAllowedToHit(Collider2D collider)
-    {
-        return !_hitMultipleTimes || !_hitDatas.ContainsKey(collider) || _hitDatas[collider].TimeSinceLastHit >= _delayBetweenHits;
     }
 }
